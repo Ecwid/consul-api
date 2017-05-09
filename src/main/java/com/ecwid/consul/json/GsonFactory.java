16993f9f2989696fe8faa67bc203e97b9fd6917f
Jeremy Pitzeruse <pitz@indeed.com>
2017-05-09 15:47:01 -0500

    #98: Fixed the GsonFactory to properly support serialization and deserialization of enumeration values annotated with @SerializedName

--- src/main/java/com/ecwid/consul/json/GsonFactory.java
+++ src/main/java/com/ecwid/consul/json/GsonFactory.java
@@ -2,13 +2,15 @@ package com.ecwid.consul.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.TypeAdapters;
 
 /**
  * @author Vasily Vasilkov (vgv@ecwid.com)
  */
 public class GsonFactory {
 
-	private static final GsonBuilder GSON_BUILDER = new GsonBuilder();
+	private static final GsonBuilder GSON_BUILDER = new GsonBuilder()
+			.registerTypeAdapterFactory(TypeAdapters.ENUM_FACTORY);
 
 	private static final ThreadLocal<Gson> GSON = new ThreadLocal<Gson>() {
 		@Override
