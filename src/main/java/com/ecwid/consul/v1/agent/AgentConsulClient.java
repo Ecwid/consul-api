feb11ce2169c1a083682ea94ef21ca5e209e76b1
yangfuhai <fuhai999@gmail.com>
2017-11-09 22:30:37 +0800

    fixed check/warn request method error

--- src/main/java/com/ecwid/consul/v1/agent/AgentConsulClient.java
+++ src/main/java/com/ecwid/consul/v1/agent/AgentConsulClient.java
@@ -206,7 +206,7 @@ public final class AgentConsulClient implements AgentClient {
 	@Override
 	public Response<Void> agentCheckWarn(String checkId, String note) {
 		UrlParameters noteParams = note != null ? new SingleUrlParameters("note", note) : null;
-		RawResponse rawResponse = rawClient.makeGetRequest("/v1/agent/check/warn/" + checkId, noteParams);
+		RawResponse rawResponse = rawClient.makePutRequest("/v1/agent/check/warn/" + checkId, noteParams);
 
 		if (rawResponse.getStatusCode() == 200) {
 			return new Response<Void>(null, rawResponse);
