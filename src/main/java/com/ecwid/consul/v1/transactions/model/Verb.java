7a5785579103c7eb784493f451c79257cc5d174f
cliven <quanguanyu@qq.com>
2019-11-28 12:15:59 +0800

    fix  enum gson serialize not match

--- src/main/java/com/ecwid/consul/v1/transactions/model/Verb.java
+++ src/main/java/com/ecwid/consul/v1/transactions/model/Verb.java
@@ -1,5 +1,7 @@
 package com.ecwid.consul.v1.transactions.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Specifies the type of operation to perform.
  *
@@ -7,54 +9,60 @@ package com.ecwid.consul.v1.transactions.model;
  */
 public enum Verb {
 
-    /**
-     * Sets the Key to the given Value
-     */
-    set("set"),
-    /**
-     * 	Sets, but with CAS semantics
-     */
-    cas("cas"),
-    /**
-     * 	Lock with the given Session
-     */
-    lock("lock"),
-    /**
-     * 	Unlock with the given Session
-     */
-    unlock("unlock"),
-    /**
-     * Get the key, fails if it does not exist
-     */
-    get("get"),
-    /**
-     * Gets all keys with the prefix
-     */
-    get_tree("get-tree"),
-    /**
+	/**
+	 * Sets the Key to the given Value
+	 */
+	set("set"),
+	/**
+	 * 	Sets, but with CAS semantics
+	 */
+	cas("cas"),
+	/**
+	 * 	Lock with the given Session
+	 */
+	lock("lock"),
+	/**
+	 * 	Unlock with the given Session
+	 */
+	unlock("unlock"),
+	/**
+	 * Get the key, fails if it does not exist
+	 */
+	get("get"),
+	/**
+	 * Gets all keys with the prefix
+	 */
+	@SerializedName("get-tree")
+	get_tree("get-tree"),
+	/**
 	 * 	Fail if modify index != index
-     */
-    check_index("check-index"),
-    /**
+	 */
+	@SerializedName("check-index")
+	check_index("check-index"),
+	/**
 	 * Fail if not locked by session
-     */
-    check_session("check-session"),
-    /**
+	 */
+	@SerializedName("check-session")
+	check_session("check-session"),
+	/**
 	 * Fail if key exists
-     */
-    check_not_exists("check-not-exists"),
-    /**
+	 */
+	@SerializedName("check-not-exists")
+	check_not_exists("check-not-exists"),
+	/**
 	 * Delete the key
-     */
-    delete("delete"),
-    /**
+	 */
+	delete("delete"),
+	/**
 	 * 	Delete all keys with a prefix
-     */
-    delete_tree("delete-tree"),
-    /**
+	 */
+	@SerializedName("delete-tree")
+	delete_tree("delete-tree"),
+	/**
 	 * Delete, but with CAS semantics
-     */
-    delete_cas("delete-cas");
+	 */
+	@SerializedName("delete-cas")
+	delete_cas("delete-cas");
 
     /**
      * Verb
