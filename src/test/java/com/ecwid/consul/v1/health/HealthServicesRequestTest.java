cc1ce7ec4a002508a857fb8baab6987f5c4d024d
Avishai Weissberg <avishai@indeed.com>
2021-05-15 09:51:36 -0700

    add support for the filter tag
    also: deprecate usage of the tag parameter for filtering

--- src/test/java/com/ecwid/consul/v1/health/HealthServicesRequestTest.java
+++ src/test/java/com/ecwid/consul/v1/health/HealthServicesRequestTest.java
@@ -1,5 +1,6 @@
 package com.ecwid.consul.v1.health;
 
+import com.ecwid.consul.v1.Filter;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,13 @@ class HealthServicesRequestTest {
 	class EqualsAndHashCode {
 		@Test
 		void shouldVerify() {
-			EqualsVerifier.forClass(HealthServicesRequest.class).verify();
+			EqualsVerifier.forClass(HealthServicesRequest.class)
+					.withPrefabValues(
+							Filter.class,
+							Filter.of(Filter.MatchingOperator.EQUAL, Filter.Selector.of("foo"), "bar"),
+							Filter.in("baz", Filter.Selector.of("fang"))
+					)
+					.verify();
 		}
 	}
 }
