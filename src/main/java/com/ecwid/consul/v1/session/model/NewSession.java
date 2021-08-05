d51c2a464ef38885bfdaf628b4098d695f4e28bf
Dan Peters <dan.peters@lmrpartners.com>
2021-08-05 15:29:26 +0100

    Add service checks and node checks to session objects

--- src/main/java/com/ecwid/consul/v1/session/model/NewSession.java
+++ src/main/java/com/ecwid/consul/v1/session/model/NewSession.java
@@ -18,8 +18,18 @@ public class NewSession {
 	@SerializedName("Node")
 	private String node;
 
+	/**
+	 * @deprecated this field is deprecated as of consul 1.7, the fields nodeChecks and serviceChecks should be used instead
+	 */
+	@Deprecated
 	@SerializedName("Checks")
 	private List<String> checks;
+
+	@SerializedName("NodeChecks")
+	private List<String> nodeChecks;
+
+	@SerializedName("ServiceChecks")
+	private List<ServiceCheck> serviceChecks;
 
 	@SerializedName("Behavior")
 	private Session.Behavior behavior;
@@ -57,6 +67,22 @@ public class NewSession {
 
 	public void setChecks(List<String> checks) {
 		this.checks = checks;
+	}
+
+	public List<String> getNodeChecks() {
+		return nodeChecks;
+	}
+
+	public void setNodeChecks(List<String> nodeChecks) {
+		this.nodeChecks = nodeChecks;
+	}
+
+	public List<ServiceCheck> getServiceChecks() {
+		return serviceChecks;
+	}
+
+	public void setServiceChecks(List<ServiceCheck> serviceChecks) {
+		this.serviceChecks = serviceChecks;
 	}
 
 	public Session.Behavior getBehavior() {
